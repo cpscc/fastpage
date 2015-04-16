eb2697cd449bd5415eeda59aeff88b42da5c0b8b
anoxic <brian@zickzickzick.com>
2015-04-16 13:02:18 +0000

    move file requires into config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+require dirname(__FILE__) . '/vendor/autoload.php';
+require dirname(__FILE__) . '/lib/root.php';
+
+foreach (glob(root("lib/*.php")) as $filename) {
+    require_once $filename;
+}
+
+ini_set('date.timezone', 'America/Los_Angeles');
+
+ini_set('display_errors', true);
+error_reporting(E_ERROR | E_PARSE);
+
+
+/** YAML Repositories */
+
+repo_initialize('public/profile');
+repo_initialize('users');
+repo_initialize('profiles');
+
--- public/index.php
+++ public/index.php
@@ -1,25 +1,9 @@
 <?php
 
-require '../vendor/autoload.php';
-
-foreach (glob("../lib/*.php") as $filename) {
-    require $filename;
-}
-
-ini_set('date.timezone', 'America/Los_Angeles');
-
-ini_set('display_errors', true);
-error_reporting(E_ERROR | E_PARSE);
+require '../config.php';
 
 session_cache_limiter(false);
 session_start(); 
-
-
-/** YAML Repositories */
-
-repo_initialize('public/profile');
-repo_initialize('users');
-repo_initialize('profiles');
 
 
 /** Set theme */
