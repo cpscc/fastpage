a158977391d3d47553a8e0ee2959ed23fe361d36
anoxic <brian@zickzickzick.com>
2015-04-21 12:04:57 -0400

    render(): add $no_echo parameter

--- lib/render.php
+++ lib/render.php
@@ -1,5 +1,5 @@
 <?php
-function render($view, array $model = [], $string_loader = false)
+function render($view, array $model = [], $string_loader = false, $no_print = false)
 {
     $options = array( 
         'pragmas'         => [Mustache_Engine::PRAGMA_FILTERS, Mustache_Engine::PRAGMA_BLOCKS],
@@ -13,6 +13,9 @@ function render($view, array $model = [], $string_loader = false)
     $model = array_merge($model, (array)$_SESSION['flash'], ['app'=>APP]);
     $_SESSION['flash'] = [];
 
-    print (new Mustache_Engine($options))->render($view, $model);
+    $render = (new Mustache_Engine($options))->render($view, $model);
+
+    if($no_print) return $render;
+    else           print $render;
 }
 
