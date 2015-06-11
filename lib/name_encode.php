a5cdcb3ea23cc9882eda031c44e0a6359e24ac69
anoxic <brian@zickzickzick.com>
2015-06-11 19:52:25 +0000

    simplify name_encode slightly

--- lib/name_encode.php
+++ lib/name_encode.php
@@ -1,7 +1,7 @@
 <?php
 function name_encode($raw)
 {
-    return preg_replace("/ +/", " ", preg_replace("/[^a-z0-9.]/", "_", strtolower($raw)));
+    return preg_replace("/ +/", " ", preg_replace("/[^a-z0-9]/", "_", strtolower($raw)));
 }
 
 
